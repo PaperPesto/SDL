1856d5e4a3e13e93a552e239ea8da7414b06175a
Leonardo Bianchini <sbwnco@gmail.com>
2020-05-05 11:57:59 +0200

    Ok rendering omino che va nella pagina

--- DrawingTextures/DrawingTextures/Game.cpp
+++ DrawingTextures/DrawingTextures/Game.cpp
@@ -2,6 +2,9 @@
 #include "Game.h"
 #include <iostream>
 
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+
 Game::Game() {}
 Game::~Game() {}
 
@@ -31,6 +34,10 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	else {
 		isRunning = false;
 	}
+
+	SDL_Surface* tmpSurface = IMG_Load("assets/player.png");
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents() {
@@ -48,11 +55,17 @@ void Game::handleEvents() {
 
 void Game::update() {
 	cnt++;
+	destR.h = 64;
+	destR.w = 64;
+	destR.x = cnt/16;
+	destR.y = cnt / 32;
+
 	std::cout << cnt << std::endl;
 }
 
 void Game::render() {
 	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
 	SDL_RenderPresent(renderer);
 }
 
---
+++ DrawingTextures/DrawingTextures/readme.txt
@@ -0,0 +1,7 @@
+Aggiungere alla root folder la libreria per SDL dalla cartella SDL2/lib/x64
+- SDL2.dll
+
+Aggiungere 3 dll (x64) alla root folder del progetto dalla cartella SDL2_image-2.0.1/lib/x64
+- libpng16-16.dll
+- SDL2_image.dll
+- zlib1.dll
