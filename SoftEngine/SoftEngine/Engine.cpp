3f147925ed58f22660470e7ff9b441d16e0e29e9
Leonardo Bianchini <sbwnco@gmail.com>
2020-05-10 11:52:03 +0200

    Inizializzazione renderer e window

--- SoftEngine/SoftEngine/Engine.cpp
+++ SoftEngine/SoftEngine/Engine.cpp
@@ -6,8 +6,47 @@
 Engine* Engine::s_Instance = nullptr;
 
 bool Engine::Init() {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0 && IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) != 0) {
+		SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+
+	m_Window = SDL_CreateWindow("Soft Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	if (m_Window == nullptr) {
+		SDL_Log("Failed to create Window: %s", SDL_GetError());
+		return false;
+	}
+
+	m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (m_Renderer == nullptr) {
+		SDL_Log("Failed to create Renderer: %s", SDL_GetError());
+		return false;
+	}
+
 	m_IsRunning = true;
 	return m_IsRunning;
+}
+
+void Engine::Update() {
+	//std::cout << "updating..." << std::endl;
+}
+
+void Engine::Render() {
+	SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+	SDL_RenderPresent(m_Renderer);
+}
+
+void Engine::Events() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+	case SDL_QUIT:
+		Quit();
+		break;
+	default:
+		break;
+	}
 }
 
 bool Engine::Clean() {
@@ -15,11 +54,5 @@ bool Engine::Clean() {
 }
 
 void Engine::Quit() {
-
+	m_IsRunning = false;
 }
-void Engine::Update() {
-	std::cout << "updating..." << std::endl;
-}
-
-void Engine::Render() {}
-void Engine::Events() {}
--- SoftEngine/SoftEngine/Engine.h
+++ SoftEngine/SoftEngine/Engine.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "SDL.h"
+#include "SDL_image.h"
+
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 640
+
 class Engine
 {
 public:
@@ -16,10 +22,15 @@ public:
 	void Events();
 
 	inline bool IsRunning() { return m_IsRunning; };
+	inline SDL_Renderer* GetRenderer() { return m_Renderer; }
 
 private:
 	Engine() {};
 	bool m_IsRunning;
+
+	SDL_Window* m_Window;
+	SDL_Renderer* m_Renderer;
+
 	static Engine* s_Instance;
 };
 
