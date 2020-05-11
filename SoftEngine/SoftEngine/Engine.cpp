aad9366c1c41dfa02c8eb5bbf965a55f2b80e23d
Leonardo Bianchini <sbwnco@gmail.com>
2020-05-11 20:47:38 +0200

    Input

--- SoftEngine/SoftEngine/Engine.cpp
+++ SoftEngine/SoftEngine/Engine.cpp
@@ -5,6 +5,7 @@
 #include "TextureManager.h"
 #include "Transform.h"
 #include "Warrior.h"
+#include "Input.h"
 
 Engine* Engine::s_Instance = nullptr;
 Warrior* player = nullptr;
@@ -38,6 +39,10 @@ bool Engine::Init() {
 }
 
 void Engine::Update() {
+	if (Input::GetInstance()->GetKeyDown(SDL_SCANCODE_A)) {
+		SDL_Log("Key A pushed!");
+	};
+
 	player->Update(0);
 }
 
@@ -51,16 +56,7 @@ void Engine::Render() {
 }
 
 void Engine::Events() {
-	SDL_Event event;
-	SDL_PollEvent(&event);
-
-	switch (event.type) {
-	case SDL_QUIT:
-		Quit();
-		break;
-	default:
-		break;
-	}
+	Input::GetInstance()->Listen();
 }
 
 bool Engine::Clean() {
