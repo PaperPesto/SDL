280318bdaa42ca45c9233c22953d194a9423c977
Leonardo Bianchini <sbwnco@gmail.com>
2020-05-13 14:39:29 +0200

    Tile Layer

---
+++ SoftEngine/SoftEngine/TileLayer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Layer.h"
+#include <string>
+#include <vector>
+
+struct TileSet {
+	int First, LastID;
+	int RowCount, ColCOunt;
+	int TileCount, TileSize;
+	std::string Name, Source;
+};
+
+using TilesetList = std::vector<TileSet>;
+using TileMap = std::vector<std::vector<int> >;
+
+class TileLayer : public Layer{
+public:
+	TileLayer(int tilesize, int rowcuont, int colcount, TileMap tilemap, TilesetList tilesets);
+	virtual void Render();
+	virtual void Update();
+	inline TileMap GetTilemap() { return m_TileMap; }
+
+private:
+	int m_TileSize;
+	int m_RowCount, m_ColCount;
+	TileMap m_TileMap;
+
+	TilesetList m_Tilesets;
+	//https://youtu.be/FbtKSU92Xz8?list=PL-K0viiuJ2RctP5nlJlqmHGeh66-GOZR_&t=1116
+};
+
